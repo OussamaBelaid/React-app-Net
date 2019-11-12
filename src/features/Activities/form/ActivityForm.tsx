8c13ed5fcc07463a983fcab150183a7b53a144ce
ob <ou91@hotmail.fr>
2019-11-12 11:30:19 +0100

    add style to the app

--- src/features/Activities/form/ActivityForm.tsx
+++ src/features/Activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import ActivityStore from "../../../app/stores/activityStore";
 import { v4 as uuid } from "uuid";
@@ -7,10 +7,13 @@ import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router";
 
 interface DetailPrams {
-  id:string;
+  id: string;
 }
 
-export const ActivityForm: React.FC<RouteComponentProps<DetailPrams>> = ({match,history}) => {
+export const ActivityForm: React.FC<RouteComponentProps<DetailPrams>> = ({
+  match,
+  history
+}) => {
   const activityStore = useContext(ActivityStore);
   const {
     createActivity,
@@ -20,10 +23,8 @@ export const ActivityForm: React.FC<RouteComponentProps<DetailPrams>> = ({match,
     loadActivity,
     clearActivity
   } = activityStore;
-  console.log(initialFormState)
- 
- 
-  
+  console.log(initialFormState);
+
   const [Activity, setActivity] = useState<IActivity>({
     id: "",
     title: "",
@@ -34,24 +35,34 @@ export const ActivityForm: React.FC<RouteComponentProps<DetailPrams>> = ({match,
     venue: ""
   });
   useEffect(() => {
-    if(match.params.id && Activity.id.length === 0)
-    {
-      loadActivity(match.params.id).then(() => initialFormState && setActivity(initialFormState)
+    if (match.params.id && Activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
       );
     }
-    return (() => {
-         clearActivity()
-    })
-  },[loadActivity,clearActivity,match.params.id,initialFormState,Activity.id.length]);
+    return () => {
+      clearActivity();
+    };
+  }, [
+    loadActivity,
+    clearActivity,
+    match.params.id,
+    initialFormState,
+    Activity.id.length
+  ]);
   const handleSubmit = () => {
     if (Activity.id.length === 0) {
       let newActivity = {
         ...Activity,
         id: uuid()
       };
-      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+      createActivity(newActivity).then(() =>
+        history.push(`/activities/${newActivity.id}`)
+      );
     } else {
-      editActivity(Activity).then(() => history.push(`/activities/${Activity.id}`));
+      editActivity(Activity).then(() =>
+        history.push(`/activities/${Activity.id}`)
+      );
     }
   };
 
@@ -62,61 +73,65 @@ export const ActivityForm: React.FC<RouteComponentProps<DetailPrams>> = ({match,
     setActivity({ ...Activity, [name]: value });
   };
   return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          onChange={handleInputChange}
-          placeholder='Title'
-          name='title'
-          value={Activity.title}
-        />
-        <Form.TextArea
-          onChange={handleInputChange}
-          rows={2}
-          placeholder='Description'
-          name='description'
-          value={Activity.description}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          placeholder='Category'
-          name='category'
-          value={Activity.category}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          type='datetime-local'
-          placeholder='Date'
-          name='date'
-          value={Activity.date}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          placeholder='City'
-          name='city'
-          value={Activity.city}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          placeholder='Venue'
-          name='venue'
-          value={Activity.venue}
-        />
-        <Button
-          loading={submitting}
-          floated='right'
-          positive
-          type='submit'
-          content='Submit'
-        />
-        <Button
-          onClick={() => history.push('/activities')}
-          floated='right'
-          type='button'
-          content='Cancel'
-        />
-      </Form>
-    </Segment>
+    <Grid>
+      <Grid.Column>
+        <Segment clearing>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              onChange={handleInputChange}
+              placeholder='Title'
+              name='title'
+              value={Activity.title}
+            />
+            <Form.TextArea
+              onChange={handleInputChange}
+              rows={2}
+              placeholder='Description'
+              name='description'
+              value={Activity.description}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              placeholder='Category'
+              name='category'
+              value={Activity.category}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              type='datetime-local'
+              placeholder='Date'
+              name='date'
+              value={Activity.date}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              placeholder='City'
+              name='city'
+              value={Activity.city}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              placeholder='Venue'
+              name='venue'
+              value={Activity.venue}
+            />
+            <Button
+              loading={submitting}
+              floated='right'
+              positive
+              type='submit'
+              content='Submit'
+            />
+            <Button
+              onClick={() => history.push("/activities")}
+              floated='right'
+              type='button'
+              content='Cancel'
+            />
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
